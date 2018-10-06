8a9e5f9a4938e47a79329d99f8c290dcf08dc177
Abinaya T <abi8255@gmail.com>
2018-10-06 21:16:24 +0530

    Added datasets Questions

--- datasets_questions/explore_enron_data.py
+++ datasets_questions/explore_enron_data.py
@@ -19,4 +19,28 @@ import pickle
 
 enron_data = pickle.load(open("../final_project/final_project_dataset.pkl", "r"))
 
+len_d = len(enron_data)
+print "data points (people) present in the dataset:" , len_d
+len_f = len(enron_data.values()[0])
+print "features available for each person in the dataset:" , len_f
 
+
+pois = 0
+for i in enron_data.values():
+ if i['poi'] == True:
+  pois += 1
+print "The number of persons of interest in the dataset:", pois
+
+poi_names = open("H:\\ML-Projects\\ud120-projects\\final_project\\poi_names.txt", "r")
+poi = poi_names.readlines()
+count = len(poi[2:])
+print "Total no of Pois overall:", count
+
+p_stock = enron_data["PRENTICE JAMES"]["total_stock_value"]
+print "total value of the stock belonging to James Prentice:", p_stock
+
+no_mail = enron_data['COLWELL WESLEY']['from_this_person_to_poi']
+print " No of email messages from Wesley Colwell to persons of interest:", no_mail
+
+s_stock = enron_data['SKILLING JEFFREY K']['exercised_stock_options']
+print "the value of stock options exercised by Jeffrey K Skilling:", s_stock
